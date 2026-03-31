7a3899c27f28354fa41b5387ee96df7058e98c78
itsfaraz <er.farazsheikh@gmail.com>
2026-03-31 23:08:48 +0530

    version: bump version v1.0.6

--- setworkllm/build.gradle.kts
+++ setworkllm/build.gradle.kts
@@ -56,7 +56,7 @@ afterEvaluate {
 
                 groupId = "com.github.itsfaraz"
                 artifactId = "ollm"
-                version = "1.0.5"
+                version = "1.0.6"
             }
             repositories {
                 mavenLocal()
